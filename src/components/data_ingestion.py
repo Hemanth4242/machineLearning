097c1e2a964d34aeb3e5199c210d02fb33dfbf10
Hemanth <hemanthkumar2177@gmail.com>
2024-12-05 22:11:03 +0530

    Hyper parameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,17 +2,27 @@
 import os  # For custom Exception
 import sys  # For custom Exception
 
-sys.path.insert(0,'/Users/apple123/EndToEndMl/machineLearning')
+sys.path.insert(0,'/Users/apple123/EndToEndMl/machineLearning/src')
+sys.path.insert(0,'/Users/apple123/EndToEndMl/machineLearning/src/exception.py')
+sys.path.insert(0,'/Users/apple123/EndToEndMl/machineLearning/src/logger.py')
+
+
 print(sys.path)
 # import src
-from src.exception import CustomException
-# from exception import CustomException
-from src.logger import logging
+# from src.exception import CustomException
+from exception import CustomException
+# from src.logger import logging
+from logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from src.components.data_transformation import DataTransformation
+# from src.components.data_transformation import DataTransformation
+from data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import Modeltrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -49,4 +59,7 @@ if __name__ == "__main__":
     train_data,test_data= obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer =Modeltrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,6 +3,8 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+sys.path.insert(0,'/Users/apple123/EndToEndMl/machineLearning')
+print(sys.path)
 from dataclasses import dataclass
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
